0461deea80e0446303777e8e56318aceb14de012
LuiiVasquez <luivasquez95@gmail.com>
2022-12-27 22:25:27 -0600

    adding routes & adding json middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from 'express'
 
+import projectRoutes from './routes/projects.routes.js'
+import tasksRoutes from './routes/tasks.routes.js'
+
 const app = express()
+
+// middlewares
+app.use(express.json())
+
+app.use(projectRoutes)
+app.use(tasksRoutes)
 
 export default app;
